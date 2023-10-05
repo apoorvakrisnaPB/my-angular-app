d71a73d66cf960cf114f4f0b8c171d4d17f249c8
Apoorva Krisna <apoorvakrisna@policybazaar.com>
2023-10-05 13:24:30 +0530

    greeting message component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyFirstComponentComponent } from './my-first-component/my-first-component.component';
+import { GreetingComponent } from './greeting/greeting.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyFirstComponentComponent
+    MyFirstComponentComponent,
+    GreetingComponent
   ],
   imports: [
     BrowserModule,
